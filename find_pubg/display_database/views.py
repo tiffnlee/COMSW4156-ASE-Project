cbec2480fa25543cdea13129d44b3d7e5816f2fa
caz2114 <caz2114@barnard.edu>
2017-10-24 18:43:26 -0400

    a failure of an attempt :)

--- find_pubg/display_database/views.py
+++ find_pubg/display_database/views.py
@@ -8,6 +8,7 @@ from django.template import loader
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from .forms import SignUpForm
 
 
 # Create your views here.
@@ -17,7 +18,7 @@ def home(request):
     return render(request, 'home.html')
 
 def user_board(request):
-	lst = User.objects.all()
+	lst = Search.objects.all()
 	template = loader.get_template('display_database/user_board.html')
 	context = {
 		'lst' : lst,
@@ -25,7 +26,7 @@ def user_board(request):
 	return HttpResponse(template.render(context, request))
 
 def user_page(request, user_id):
-	user= User.objects.get(user_id = user_id)
+	user= Search.objects.get(user_id = user_id)
 	response = ("UserId: %s <br> SteamId: %s <br> Email: %s")
 	return HttpResponse(response % (user.user_id, user.steam_id,user.email))
 
@@ -33,12 +34,15 @@ def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user=form.save()
+            user.refresh_from_db()
+            user.profile.birth_date = form.cleaned_data.get('birth_date')
             username = form.cleaned_data.get('username')
+            user.save()
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
