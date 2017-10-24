5db80b6d9f252f5507bac2709b09cb8543359489
onwodoh <ocn2000@barnard.edu>
2017-10-24 00:04:58 -0400

    some renaming done

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # COMSW4156-ASE-Project
+Main features of project:
+	User class (in display_database/models.py):
+		defines the attributes of a user
+	User board (template in display_database/templates/display_database.user_board.html)
+		an html page displaying all the users of the system
+	User page
+		an html page displaying the information of a user
--- find_pubg/display_database/urls.py
+++ find_pubg/display_database/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.display_users, name='display_users'),
+	url(r'^$', views.index, name='index'),
 	url(r'^(?P<user_id>[a-zA-Z0-9_]+)/user_page/$', views.user_page, name='user_page'),
-	url(r'^board_info/$', views.board_info, name='board_info'),
+	url(r'^user_board/$', views.user_board, name='user_board'),
 ]
 
 
