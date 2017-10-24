536f65a35ebc0639de0392c4c5908ae547a240f6
Tiffany Lee <tl2606@barnard.edu>
2017-10-24 17:48:43 -0400

    changes to model to reflect user

--- find_pubg/display_database/models.py
+++ find_pubg/display_database/models.py
@@ -2,9 +2,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class User(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	user_id = models.CharField(max_length=20)
 	steam_id = models.CharField(max_length=20)
 	password = models.CharField(max_length=20)
