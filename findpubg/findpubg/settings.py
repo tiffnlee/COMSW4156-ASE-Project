a2beebdc92a57dc168098829beb53eeab7e34f24
Tiffany Lee <tl2606@barnard.edu>
2017-11-25 23:04:22 -0500

    life ending addition to setting removed

--- findpubg/findpubg/settings.py
+++ findpubg/findpubg/settings.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.sites',
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'findpubg.core',
