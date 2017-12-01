df4abc52ff38e3f1916292fb41acad5c2fc7e618
Tiffany Lee <tl2606@barnard.edu>
2017-11-30 22:06:05 -0500

    fixed problem with URL.py having extra ;/

--- findpubg/findpubg/urls.py
+++ findpubg/findpubg/urls.py
@@ -18,13 +18,10 @@ urlpatterns = [
     url(r'^signup/$', core_views.signup, name='signup'),
     url(r'^search/$', core_views.search_survey, name='search'),
     url(r'^start/$', core_views.start, name='start'),
-    url(r'^user_board/sort_by_date_joined/$', core_views.sort_search_by_date_joined,;\
-    name='sort_by_date_joined'),
-	url(r'^user_board/sort_by_region_preference/$', core_views.sort_by_region_preference,;\
-    name='sort_by_region_preference'),
+    url(r'^user_board/sort_by_date_joined/$', core_views.sort_search_by_date_joined, name='sort_by_date_joined'),
+	url(r'^user_board/sort_by_region_preference/$', core_views.sort_by_region_preference, name='sort_by_region_preference'),
 	url(r'^admin/', include(admin.site.urls)),
-	url(r'^user_board/sort_by_team_preference/$', core_views.sort_by_team_preference,;\
-    name='sort_by_team_preference'),
+	url(r'^user_board/sort_by_team_preference/$', core_views.sort_by_team_preference,name='sort_by_team_preference'),
     # Apps
     url(r'^forum/', include(board.urls)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
