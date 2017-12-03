40a9ba843ae6fd88e3a2c61c58a6163997fff1fd
Tiffany Lee <tl2606@barnard.edu>
2017-12-02 23:31:03 -0500

    modified for heroku deployment

--- findpubg/findpubg/settings.py
+++ findpubg/findpubg/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     'haystack',
     'widget_tweaks',
     'django_jenkins',
+    'whitenoise',
+    'dj_database_url'
 ] + get_machina_apps()
 
 MIDDLEWARE = [
@@ -145,3 +147,23 @@ MACHINA_DEFAULT_AUTHENTICATED_USER_FORUM_PERMISSIONS = [
     'can_vote_in_polls',
     'can_download_file',
 ]
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
